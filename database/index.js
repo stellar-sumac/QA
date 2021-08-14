8391b00f6013f2025deb29e9be2011cf4b7d1216
TimAdeyemo <Timadeyemo@gmail.com>
2021-08-14 11:15:28 -0400

    Refractored to ng

--- database/index.js
+++ database/index.js
@@ -1,13 +1,41 @@
-const postgres = require('postgres');
+//const postgres = require('postgres');
+const { Pool } = require('pg');
 const path = require('path');
 const config = require('./config');
+const fs = require('fs');
+const readline = require('readline');
 
-const sql = postgres(config);
 
-sql.file(path.join(__dirname, 'schema.sql'), [], {
-  cache: true,
-});
 
-console.log('success');
+const pool = new Pool(config);
+// const sql = postgres(config);
 
-module.exports = sql;
+// sql.file(path.join(__dirname, 'schema.sql'), [], {
+//   cache: true,
+// });
+//   .then(() => console.log('success'))
+//   .catch(e => console.error(e));
+
+const readSqlBySemi = filename =>
+   fs.readFileSync(filename)
+   .toString('UTF8')
+   .split(';');
+
+const commands = readSqlBySemi(path.join(__dirname, 'schema.sql'))
+const toExecute = commands.map(command => pool.query(command));
+
+Promise.all(toExecute)
+  .then(() => console.log('success'))
+  .then(() => pool.end())
+  .catch(e => console.error(e));
+
+
+
+
+// commands.forEach(command => {
+//   pool.query(command)
+// })
+
+
+
+module.exports = pool;
